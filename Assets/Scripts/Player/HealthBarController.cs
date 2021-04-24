747d1d18cee79cd80f5681add1c07e6e466b2fc9
Kevin-Beck <kab5417@gmail.com>
2021-04-24 11:32:49 -0700

    Added and fixed health bar issues
    Had issue where prefab changes were not being saved, known unity bug with ui elements

--- Assets/Scripts/Player/HealthBarController.cs
+++ Assets/Scripts/Player/HealthBarController.cs
@@ -14,6 +14,7 @@ public class HealthBarController : MonoBehaviour
     private void Start()
     {
         HealthBarImage = GetComponent<Image>();
+        UpdateHealthBar();
     }
 
     public void UpdateHealthBar()
